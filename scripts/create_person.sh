22535745d5f5b4370654d38419ee5de927f3943b
alahr <eyibinghe@sina.com.cn>
2020-05-11 20:22:54 +0800

    push es script

---
+++ scripts/create_person.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+create_index(){
+	curl -X PUT "192.168.65.11:9200/t_person?pretty" -H 'Content-Type: application/json' -d'
+	{
+	  "mappings": {
+		"properties": {
+			"name": {
+				"type": "text"
+			},
+			"birthday": {
+				"type": "date",
+				"format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+			},
+			"address": {
+				"type": "text"
+			},
+			"createdTime": {
+				"type": "date",
+				"format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+			},
+			"modifiedTime": {
+				"type": "date",
+				"format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+			}
+		}
+	  }
+	}
+	'
+}
+
+add_alias(){
+	curl -X POST "192.168.65.11:9200/_aliases?pretty" -H 'Content-Type: application/json' -d'
+	{
+		"actions" : [
+			{ "add" : { "index" : "t_person", "alias" : "a_person" } }
+		]
+	}
+'
+}
+
+
+create_index
+echo -e "\ncreate_index ok"
+sleep 1
+
+add_alias
+echo -e "\nadd_alias ok"
+
+
+
+
+
+
